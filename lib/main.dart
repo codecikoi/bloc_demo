a206e6e7cb3a29d080d92c0ba9265d29b0d8cef1
Zoir I <wowdobryy@gmail.com>
2022-10-29 18:06:56 +0600

    using stream

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:bloc_demo/color_bloc.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'color_stream.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,47 +12,59 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: BlocProvider(
-        create: (context) => ColorBloc(),
-        child: MyHomePage(),
-      ),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
   @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  final ColorBloc _bloc = ColorBloc();
+
+  @override
+  void dispose() {
+    _bloc.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    ColorBloc bloc = BlocProvider.of<ColorBloc>(context);
-    
     return Scaffold(
       appBar: AppBar(
-        title: Text('Using flutter_bloc'),
+        title: Text('Using Stream'),
         backgroundColor: Colors.indigo[300],
         centerTitle: true,
       ),
       body: Center(
-        child: BlocBuilder<ColorBloc, Color>(
-          builder: (context, currentColor) => AnimatedContainer(
-            duration: Duration(milliseconds: 500),
-            color: currentColor,
-            height: 130,
-            width: 130,
-          ),
-        ),
+        child: StreamBuilder(
+            stream: _bloc.outputStateStream,
+            initialData: Colors.red,
+            builder: (context, AsyncSnapshot<Color> snapshot) {
+              return AnimatedContainer(
+                duration: Duration(milliseconds: 500),
+                color: snapshot.data,
+                height: 130,
+                width: 130,
+              );
+            }),
       ),
       floatingActionButton: Row(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
           FloatingActionButton(
-            onPressed: () => bloc.add(RedColorEvent()),
+            onPressed: () => _bloc.inputEventSink.add(ColorEvent.eventRed),
             backgroundColor: Colors.red,
           ),
           SizedBox(width: 10.0),
           FloatingActionButton(
-            onPressed: () => bloc.add(GreenColorEvent()),
+            onPressed: () => _bloc.inputEventSink.add(ColorEvent.eventGreen),
             backgroundColor: Colors.green,
           ),
         ],
